cc8954e0965b57857bd031380158a20369dbe01f
Weqil2025 <ar5ipooshka@yandex.ru>
2025-12-24 12:41:47 +0500

    Enhance Angular project configuration and integrate Tailwind CSS. Updated angular.json to skip tests for various schematics, added file replacements for environment configurations, and modified package.json and package-lock.json to include Tailwind CSS dependencies. Updated styles to import Tailwind CSS and refactored app.component to use a new layout component. Removed app.component.spec.ts as part of the cleanup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageContainerComponent } from './shared/components/layouts/page-container/page-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
