e1f87348ccb4085f9568b9f7cf56f8344863b993
Weqil2025 <ar5ipooshka@yandex.ru>
2026-01-13 19:03:01 +0500

    fix: update consent text for clarity and improve formatting in various components

--- src/app/core/api/auth/anonymous-auth-api.service.ts
+++ src/app/core/api/auth/anonymous-auth-api.service.ts
@@ -1,17 +1,16 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { AnonymousAuthApiInterface } from "./models/anonymous-auth.api.interface";
-import { Observable } from "rxjs";
-import { environment } from "../../../../environments/environment";
+import { HttpClient } from '@angular/common/http'
+import { inject, Injectable } from '@angular/core'
+import { AnonymousAuthApiInterface } from './models/anonymous-auth.api.interface'
+import { Observable } from 'rxjs'
+import { environment } from '../../../../environments/environment'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnonymousAuthApiService {
-    private http = inject(HttpClient);
+  private http = inject(HttpClient)
 
-    public createAnonymousAuth(): Observable<AnonymousAuthApiInterface> {
-        return this.http.post<AnonymousAuthApiInterface>(`${environment.apiUrl}/licenses/visitors`, {});
-    }
-
+  public createAnonymousAuth(): Observable<AnonymousAuthApiInterface> {
+    return this.http.post<AnonymousAuthApiInterface>(`${environment.apiUrl}/licenses/visitors`, {})
+  }
 }
